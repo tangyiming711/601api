1dfb30503b77b3a3882f94e85de13b6917e10a6e
tangyiming711 <43041771+tangyiming711@users.noreply.github.com>
2018-11-30 15:32:16 -0500

    python mongodb interface

---
+++ python_mongo.py
@@ -0,0 +1,34 @@
+
+#!/usr/bin/python3
+ 
+import pymongo
+
+#mongodb account, database, table
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mlp"]
+mycol = mydb["first"]
+#the whole table
+print("the whole information in table first:") 
+for x in mycol.find():
+  print(x)
+print("\n")
+
+#show the information about account_id and picture number
+print("the account id and picture number:")
+for x in mycol.find({},{ "_id": 0, "account_id": 1, "picture": 1 }):
+  print(x)
+print("\n")
+
+#search for a word
+myquery = { "keyword": "singer" }
+ 
+mydoc = mycol.find(myquery)
+print("the information where keyword is singer:")
+for x in mydoc:
+  print(x)  
+print("\n")
+
+#most popular descriptors
+print("the most popular descriptors:")
+for x in mycol.find({},{'_id':0,'keyword':1}):
+	print(x)
